1f4c0e350f1dfd28e76cc967a633cc372450331a
Alexey Charkov <alchark@flipper.net>
2026-03-25 11:50:54 +0400

    WIP: Input: add support for Flipper One buttons and touchpad
    Signed-off-by: Alexey Charkov <alchark@flipper.net>

---
+++ drivers/input/misc/flipper-one-input.c
@@ -0,0 +1,210 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Flipper One builtin buttons and touchpad driver
+ * Copyright (C) 2026 Flipper FZCO
+ */
+
+#include <linux/input.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/limits.h>
+#include <linux/platform_device.h>
+#include <linux/regmap.h>
+#include <linux/unaligned.h>
+
+#include <linux/mfd/flipper-one-mcu.h>
+
+#define FO_BTN_VIEW		BIT(0)
+#define FO_BTN_ESCAPE		BIT(1)
+#define FO_BTN_POWER		BIT(2)
+#define FO_BTN_EDIT		BIT(3)
+#define FO_BTN_RUN		BIT(4)
+#define FO_BTN_APPSELECT	BIT(5)
+#define FO_BTN_BACK		BIT(6)
+#define FO_BTN_DOWN		BIT(7)
+#define FO_BTN_RIGHT		BIT(8)
+#define FO_BTN_CENTER		BIT(9)
+#define FO_BTN_LEFT		BIT(10)
+#define FO_BTN_UP		BIT(11)
+#define FO_BTN_PTT		BIT(12)
+
+struct fo_input {
+	struct input_dev *idev_btn;
+	struct input_dev *idev_touch;
+	struct fomcu_device *fomcu;
+};
+
+struct fo_irq {
+	const char *name;
+	irqreturn_t (*handler)(int, void *);
+};
+
+static irqreturn_t fo_input_btn_handler(int irq, void *data)
+{
+	struct fo_input *input = data;
+	struct regmap *regmap = input->fomcu->regmap;
+	struct input_dev *idev = input->idev_btn;
+	struct device *parent = idev->dev.parent;
+	unsigned int btns;
+	int err;
+
+	err = regmap_read(regmap, FOMCU_REG_INPUT_BTNS, &btns);
+	if (err) {
+		dev_err(parent, "Failed to read button states: %d\n", err);
+		return IRQ_NONE;
+	}
+
+	input_report_key(idev, KEY_K, FO_BTN_CENTER & btns);
+	input_report_key(idev, KEY_I, FO_BTN_UP & btns);
+	input_report_key(idev, KEY_M, FO_BTN_DOWN & btns);
+	input_report_key(idev, KEY_J, FO_BTN_LEFT & btns);
+	input_report_key(idev, KEY_L, FO_BTN_RIGHT & btns);
+	input_report_key(idev, KEY_H, FO_BTN_APPSELECT & btns);
+	input_report_key(idev, KEY_N, FO_BTN_BACK & btns);
+	input_report_key(idev, KEY_A, FO_BTN_PTT & btns);
+	input_report_key(idev, KEY_Z, FO_BTN_ESCAPE & btns);
+	input_report_key(idev, KEY_X, FO_BTN_VIEW & btns);
+	input_report_key(idev, KEY_C, FO_BTN_POWER & btns);
+	input_report_key(idev, KEY_V, FO_BTN_EDIT & btns);
+	input_report_key(idev, KEY_B, FO_BTN_RUN & btns);
+	input_sync(idev);
+
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t fo_input_touch_handler(int irq, void *data)
+{
+	struct fo_input *input = data;
+	struct regmap *regmap = input->fomcu->regmap;
+	struct input_dev *idev = input->idev_touch;
+	struct device *parent = idev->dev.parent;
+	uint16_t buf[3];
+	int err;
+
+	err = regmap_bulk_read(regmap, FOMCU_REG_INPUT_TOUCH_X, &buf, ARRAY_SIZE(buf));
+	if (err) {
+		dev_err(parent, "Failed to read touch inputs: %d\n", err);
+		return IRQ_NONE;
+	}
+
+	input_report_key(idev, BTN_TOUCH, !!buf[2]);
+	input_report_key(idev, BTN_TOOL_FINGER, !!buf[2]);
+	input_report_abs(idev, ABS_X, buf[0]);
+	input_report_abs(idev, ABS_Y, buf[1]);
+	input_report_abs(idev, ABS_PRESSURE, buf[2]);
+	input_sync(idev);
+
+	return IRQ_HANDLED;
+}
+
+static const struct fo_irq fo_irqs[] = {
+	{ .name = "flipper-one-input-btn", .handler = fo_input_btn_handler },
+	{ .name = "flipper-one-input-touch", .handler = fo_input_touch_handler },
+};
+
+static int fo_input_probe(struct platform_device *pdev)
+{
+	struct fomcu_device *fomcu = dev_get_drvdata(pdev->dev.parent);
+	struct device *dev = &pdev->dev;
+	struct fo_input *input;
+	struct input_dev *idev_btn, *idev_touch;
+	int irq, err, i;
+
+	input = devm_kzalloc(dev, sizeof(*input), GFP_KERNEL);
+	if (!input)
+		return -ENOMEM;
+
+	input->fomcu = fomcu;
+
+	idev_btn = devm_input_allocate_device(dev);
+	if (!idev_btn) {
+		dev_err(dev, "Failed to allocate buttons input device\n");
+		return -ENOMEM;
+	}
+	input->idev_btn = idev_btn;
+
+	idev_btn->name = "Flipper One Buttons";
+	idev_btn->phys = "flipper-one-input/input0";
+	idev_btn->id.bustype = BUS_I2C;
+
+	idev_touch = devm_input_allocate_device(dev);
+	if (!idev_touch) {
+		dev_err(dev, "Failed to allocate touch input device\n");
+		return -ENOMEM;
+	}
+	input->idev_touch = idev_touch;
+
+	idev_touch->name = "Flipper One Touchpad";
+	idev_touch->phys = "flipper-one-input/input1";
+	idev_touch->id.bustype = BUS_I2C;
+
+	/* Buttons */
+	input_set_capability(idev_btn, EV_KEY, KEY_K);	/* D-pad center */
+	input_set_capability(idev_btn, EV_KEY, KEY_I);	/* D-pad up */
+	input_set_capability(idev_btn, EV_KEY, KEY_M);	/* D-pad down */
+	input_set_capability(idev_btn, EV_KEY, KEY_J);	/* D-pad left */
+	input_set_capability(idev_btn, EV_KEY, KEY_L);	/* D-pad right */
+	input_set_capability(idev_btn, EV_KEY, KEY_H);	/* App switcher */
+	input_set_capability(idev_btn, EV_KEY, KEY_N);	/* Back */
+	input_set_capability(idev_btn, EV_KEY, KEY_A);	/* PTT */
+	input_set_capability(idev_btn, EV_KEY, KEY_Z);	/* Escape */
+	input_set_capability(idev_btn, EV_KEY, KEY_X);	/* View */
+	input_set_capability(idev_btn, EV_KEY, KEY_C);	/* Power */
+	input_set_capability(idev_btn, EV_KEY, KEY_V);	/* Edit */
+	input_set_capability(idev_btn, EV_KEY, KEY_B);	/* Run */
+
+	/* Touchpad */
+	input_set_capability(idev_touch, EV_KEY, BTN_TOUCH);
+	input_set_capability(idev_touch, EV_KEY, BTN_TOOL_FINGER);
+	input_set_abs_params(idev_touch, ABS_X, 0, 1024, 0, 0);
+	input_set_abs_params(idev_touch, ABS_Y, 0, 800, 0, 0);
+	input_set_abs_params(idev_touch, ABS_PRESSURE, 0, 12288, 0, 0);
+
+	__set_bit(INPUT_PROP_POINTER, idev_touch->propbit);
+
+	device_set_wakeup_capable(dev, true);
+	device_wakeup_enable(dev);
+
+	for (i = 0; i < ARRAY_SIZE(fo_irqs); i++) {
+		irq = platform_get_irq_byname(pdev, fo_irqs[i].name);
+		if (irq < 0)
+			return dev_err_probe(dev, irq, "Failed to get IRQ %s\n",
+					     fo_irqs[i].name);
+
+		err = devm_request_threaded_irq(dev, irq, NULL, fo_irqs[i].handler,
+						IRQF_ONESHOT | IRQF_NO_SUSPEND,
+						fo_irqs[i].name, input);
+		if (err)
+			return dev_err_probe(dev, err, "Failed to request IRQ %s\n",
+					     fo_irqs[i].name);
+	}
+
+	err = input_register_device(idev_btn);
+	if (err)
+		return dev_err_probe(dev, err, "Failed to register buttons input device\n");
+
+	err = input_register_device(idev_touch);
+	if (err)
+		return dev_err_probe(dev, err, "Failed to register touch input device\n");
+
+	return 0;
+}
+
+static const struct platform_device_id fo_input_id_table[] = {
+	{ "flipper-one-input", },
+	{ }
+};
+MODULE_DEVICE_TABLE(platform, fo_input_id_table);
+
+static struct platform_driver fo_input_driver = {
+	.driver = {
+		.name = "flipper-one-input",
+	},
+	.probe = fo_input_probe,
+	.id_table = fo_input_id_table,
+};
+module_platform_driver(fo_input_driver);
+
+MODULE_DESCRIPTION("Flipper One buttons and touchpad driver");
+MODULE_AUTHOR("Alexey Charkov <alchark@flipper.net>");
+MODULE_LICENSE("GPL");
