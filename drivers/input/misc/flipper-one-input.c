02619649d1a6656432ed2228728b01652d9c18fc
Alexey Charkov <alchark@flipper.net>
2026-06-22 18:37:11 +0400

    Input: flipper-one-input: add support for SW buttons
    Extend the flipper-one-input driver to support the MCU software driven
    buttons. While at that, refactor the code to reduce duplication.
    
    Signed-off-by: Alexey Charkov <alchark@flipper.net>

--- drivers/input/misc/flipper-one-input.c
+++ drivers/input/misc/flipper-one-input.c
@@ -4,14 +4,20 @@
  * Copyright (C) 2026 Flipper FZCO
  */
 
-#include <linux/input.h>
+#include <linux/array_size.h>
+#include <linux/bitops.h>
+#include <linux/bits.h>
+#include <linux/device.h>
+#include <linux/errno.h>
 #include <linux/input-event-codes.h>
+#include <linux/input.h>
 #include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <linux/limits.h>
+#include <linux/mod_devicetable.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/pm_wakeup.h>
 #include <linux/regmap.h>
-#include <linux/unaligned.h>
+#include <linux/types.h>
 
 #include <linux/mfd/flipper-one-mcu.h>
 
@@ -36,23 +42,12 @@
 #define FO_HS_BTN_C		BIT(4)
 #define FO_HS_BTN_D		BIT(5)
 
-struct fo_input {
-	struct input_dev *idev_btn;
-	struct input_dev *idev_touch;
-	struct input_dev *idev_headset;
-	struct fomcu_device *fomcu;
-};
-
-struct fo_irq {
-	const char *name;
-	irqreturn_t (*handler)(int, void *);
-};
+#define FO_SWBTN_POWER		BIT(0)
 
 static irqreturn_t fo_input_btn_handler(int irq, void *data)
 {
-	struct fo_input *input = data;
-	struct regmap *regmap = input->fomcu->regmap;
-	struct input_dev *idev = input->idev_btn;
+	struct input_dev *idev = data;
+	struct regmap *regmap = input_get_drvdata(idev);
 	struct device *parent = idev->dev.parent;
 	unsigned int reg;
 	int err;
@@ -81,13 +76,29 @@ static irqreturn_t fo_input_btn_handler(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
+static void fo_input_setup_btn(struct input_dev *idev)
+{
+	input_set_capability(idev, EV_KEY, KEY_ENTER);		/* D-pad center */
+	input_set_capability(idev, EV_KEY, KEY_UP);		/* D-pad up */
+	input_set_capability(idev, EV_KEY, KEY_DOWN);		/* D-pad down */
+	input_set_capability(idev, EV_KEY, KEY_LEFT);		/* D-pad left */
+	input_set_capability(idev, EV_KEY, KEY_RIGHT);		/* D-pad right */
+	input_set_capability(idev, EV_KEY, KEY_TAB);		/* App switcher */
+	input_set_capability(idev, EV_KEY, KEY_BACKSPACE);	/* Back */
+	input_set_capability(idev, EV_KEY, KEY_A);		/* PTT */
+	input_set_capability(idev, EV_KEY, KEY_Z);		/* Escape */
+	input_set_capability(idev, EV_KEY, KEY_X);		/* View */
+	input_set_capability(idev, EV_KEY, KEY_C);		/* Power */
+	input_set_capability(idev, EV_KEY, KEY_V);		/* Edit */
+	input_set_capability(idev, EV_KEY, KEY_B);		/* Run */
+}
+
 static irqreturn_t fo_input_touch_handler(int irq, void *data)
 {
-	struct fo_input *input = data;
-	struct regmap *regmap = input->fomcu->regmap;
-	struct input_dev *idev = input->idev_touch;
+	struct input_dev *idev = data;
+	struct regmap *regmap = input_get_drvdata(idev);
 	struct device *parent = idev->dev.parent;
-	uint16_t buf[3];
+	u16 buf[3];
 	int err;
 
 	err = regmap_bulk_read(regmap, FOMCU_REG_INPUT_TOUCH_X, &buf, ARRAY_SIZE(buf));
@@ -106,11 +117,20 @@ static irqreturn_t fo_input_touch_handler(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
+static void fo_input_setup_touch(struct input_dev *idev)
+{
+	input_set_capability(idev, EV_KEY, BTN_TOUCH);
+	input_set_capability(idev, EV_KEY, BTN_TOOL_FINGER);
+	input_set_abs_params(idev, ABS_X, 0, 1024, 0, 0);
+	input_set_abs_params(idev, ABS_Y, 0, 800, 0, 0);
+	input_set_abs_params(idev, ABS_PRESSURE, 0, 12288, 0, 0);
+	__set_bit(INPUT_PROP_POINTER, idev->propbit);
+}
+
 static irqreturn_t fo_input_headset_handler(int irq, void *data)
 {
-	struct fo_input *input = data;
-	struct regmap *regmap = input->fomcu->regmap;
-	struct input_dev *idev = input->idev_headset;
+	struct input_dev *idev = data;
+	struct regmap *regmap = input_get_drvdata(idev);
 	struct device *parent = idev->dev.parent;
 	unsigned int reg;
 	int err;
@@ -132,118 +152,124 @@ static irqreturn_t fo_input_headset_handler(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
-static const struct fo_irq fo_irqs[] = {
-	{ .name = "flipper-one-input-btn", .handler = fo_input_btn_handler },
-	{ .name = "flipper-one-input-touch", .handler = fo_input_touch_handler },
-	{ .name = "flipper-one-input-headset", .handler = fo_input_headset_handler },
+static void fo_input_setup_headset(struct input_dev *idev)
+{
+	input_set_capability(idev, EV_SW, SW_HEADPHONE_INSERT);
+	input_set_capability(idev, EV_SW, SW_MICROPHONE_INSERT);
+	input_set_capability(idev, EV_KEY, KEY_PLAYPAUSE);
+	input_set_capability(idev, EV_KEY, KEY_VOLUMEUP);
+	input_set_capability(idev, EV_KEY, KEY_VOLUMEDOWN);
+	input_set_capability(idev, EV_KEY, KEY_VOICECOMMAND);
+}
+
+static irqreturn_t fo_input_swbtn_handler(int irq, void *data)
+{
+	struct input_dev *idev = data;
+	struct regmap *regmap = input_get_drvdata(idev);
+	struct device *parent = idev->dev.parent;
+	unsigned int reg;
+	int err;
+
+	err = regmap_read(regmap, FOMCU_REG_INPUT_SWBTNS, &reg);
+	if (err) {
+		dev_err(parent, "Failed to read SW button states: %d\n", err);
+		return IRQ_NONE;
+	}
+
+	input_report_key(idev, KEY_POWER, FO_SWBTN_POWER & reg);
+	input_sync(idev);
+
+	return IRQ_HANDLED;
+}
+
+static void fo_input_setup_swbtn(struct input_dev *idev)
+{
+	input_set_capability(idev, EV_KEY, KEY_POWER);
+}
+
+struct fo_input_config {
+	const char *name;
+	const char *phys;
+	const char *irq_name;
+	irqreturn_t (*handler)(int irq, void *data);
+	void (*setup)(struct input_dev *idev);
+};
+
+static const struct fo_input_config fo_input_configs[] = {
+	{
+		.name = "Flipper One Buttons",
+		.phys = "flipper-one-input/input0",
+		.irq_name = "flipper-one-input-btn",
+		.handler = fo_input_btn_handler,
+		.setup = fo_input_setup_btn,
+	},
+	{
+		.name = "Flipper One Touchpad",
+		.phys = "flipper-one-input/input1",
+		.irq_name = "flipper-one-input-touch",
+		.handler = fo_input_touch_handler,
+		.setup = fo_input_setup_touch,
+	},
+	{
+		.name = "Flipper One Headset",
+		.phys = "flipper-one-input/input2",
+		.irq_name = "flipper-one-input-headset",
+		.handler = fo_input_headset_handler,
+		.setup = fo_input_setup_headset,
+	},
+	{
+		.name = "Flipper One Software Buttons",
+		.phys = "flipper-one-input/input3",
+		.irq_name = "flipper-one-input-swbtn",
+		.handler = fo_input_swbtn_handler,
+		.setup = fo_input_setup_swbtn,
+	},
 };
 
 static int fo_input_probe(struct platform_device *pdev)
 {
 	struct fomcu_device *fomcu = dev_get_drvdata(pdev->dev.parent);
 	struct device *dev = &pdev->dev;
-	struct fo_input *input;
-	struct input_dev *idev_btn, *idev_touch, *idev_headset;
 	int irq, err, i;
 
-	input = devm_kzalloc(dev, sizeof(*input), GFP_KERNEL);
-	if (!input)
-		return -ENOMEM;
+	err = devm_device_init_wakeup(dev);
+	if (err)
+		return dev_err_probe(dev, err, "Failed to init wakeup\n");
 
-	input->fomcu = fomcu;
+	for (i = 0; i < ARRAY_SIZE(fo_input_configs); i++) {
+		const struct fo_input_config *cfg = &fo_input_configs[i];
+		struct input_dev *idev;
 
-	idev_btn = devm_input_allocate_device(dev);
-	if (!idev_btn) {
-		dev_err(dev, "Failed to allocate buttons input device\n");
-		return -ENOMEM;
-	}
-	input->idev_btn = idev_btn;
+		idev = devm_input_allocate_device(dev);
+		if (!idev)
+			return dev_err_probe(dev, -ENOMEM,
+					     "Failed to allocate %s input device\n",
+					     cfg->name);
 
-	idev_btn->name = "Flipper One Buttons";
-	idev_btn->phys = "flipper-one-input/input0";
-	idev_btn->id.bustype = BUS_I2C;
+		idev->name = cfg->name;
+		idev->phys = cfg->phys;
+		idev->id.bustype = BUS_I2C;
+		input_set_drvdata(idev, fomcu->regmap);
+		cfg->setup(idev);
 
-	idev_touch = devm_input_allocate_device(dev);
-	if (!idev_touch) {
-		dev_err(dev, "Failed to allocate touch input device\n");
-		return -ENOMEM;
-	}
-	input->idev_touch = idev_touch;
-
-	idev_touch->name = "Flipper One Touchpad";
-	idev_touch->phys = "flipper-one-input/input1";
-	idev_touch->id.bustype = BUS_I2C;
-
-	idev_headset = devm_input_allocate_device(dev);
-	if (!idev_headset) {
-		dev_err(dev, "Failed to allocate headset input device\n");
-		return -ENOMEM;
-	}
-	input->idev_headset = idev_headset;
-
-	idev_headset->name = "Flipper One Headset";
-	idev_headset->phys = "flipper-one-input/input2";
-	idev_headset->id.bustype = BUS_I2C;
-
-	/* Buttons */
-	input_set_capability(idev_btn, EV_KEY, KEY_ENTER);	/* D-pad center */
-	input_set_capability(idev_btn, EV_KEY, KEY_UP);	/* D-pad up */
-	input_set_capability(idev_btn, EV_KEY, KEY_DOWN);	/* D-pad down */
-	input_set_capability(idev_btn, EV_KEY, KEY_LEFT);	/* D-pad left */
-	input_set_capability(idev_btn, EV_KEY, KEY_RIGHT);	/* D-pad right */
-	input_set_capability(idev_btn, EV_KEY, KEY_TAB);	/* App switcher */
-	input_set_capability(idev_btn, EV_KEY, KEY_BACKSPACE);	/* Back */
-	input_set_capability(idev_btn, EV_KEY, KEY_A);	/* PTT */
-	input_set_capability(idev_btn, EV_KEY, KEY_Z);	/* Escape */
-	input_set_capability(idev_btn, EV_KEY, KEY_X);	/* View */
-	input_set_capability(idev_btn, EV_KEY, KEY_C);	/* Power */
-	input_set_capability(idev_btn, EV_KEY, KEY_V);	/* Edit */
-	input_set_capability(idev_btn, EV_KEY, KEY_B);	/* Run */
-
-	/* Touchpad */
-	input_set_capability(idev_touch, EV_KEY, BTN_TOUCH);
-	input_set_capability(idev_touch, EV_KEY, BTN_TOOL_FINGER);
-	input_set_abs_params(idev_touch, ABS_X, 0, 1024, 0, 0);
-	input_set_abs_params(idev_touch, ABS_Y, 0, 800, 0, 0);
-	input_set_abs_params(idev_touch, ABS_PRESSURE, 0, 12288, 0, 0);
-	__set_bit(INPUT_PROP_POINTER, idev_touch->propbit);
-
-	/* Headset */
-	input_set_capability(idev_headset, EV_SW, SW_HEADPHONE_INSERT);
-	input_set_capability(idev_headset, EV_SW, SW_MICROPHONE_INSERT);
-	input_set_capability(idev_headset, EV_KEY, KEY_PLAYPAUSE);
-	input_set_capability(idev_headset, EV_KEY, KEY_VOLUMEUP);
-	input_set_capability(idev_headset, EV_KEY, KEY_VOLUMEDOWN);
-	input_set_capability(idev_headset, EV_KEY, KEY_VOICECOMMAND);
-
-	device_set_wakeup_capable(dev, true);
-	device_wakeup_enable(dev);
-
-	for (i = 0; i < ARRAY_SIZE(fo_irqs); i++) {
-		irq = platform_get_irq_byname(pdev, fo_irqs[i].name);
+		irq = platform_get_irq_byname(pdev, cfg->irq_name);
 		if (irq < 0)
 			return dev_err_probe(dev, irq, "Failed to get IRQ %s\n",
-					     fo_irqs[i].name);
+					     cfg->irq_name);
 
-		err = devm_request_threaded_irq(dev, irq, NULL, fo_irqs[i].handler,
+		err = devm_request_threaded_irq(dev, irq, NULL, cfg->handler,
 						IRQF_ONESHOT | IRQF_NO_SUSPEND,
-						fo_irqs[i].name, input);
+						cfg->irq_name, idev);
 		if (err)
 			return dev_err_probe(dev, err, "Failed to request IRQ %s\n",
-					     fo_irqs[i].name);
+					     cfg->irq_name);
+
+		err = input_register_device(idev);
+		if (err)
+			return dev_err_probe(dev, err,
+					     "Failed to register %s input device\n",
+					     cfg->name);
 	}
-
-	err = input_register_device(idev_btn);
-	if (err)
-		return dev_err_probe(dev, err, "Failed to register buttons input device\n");
-
-	err = input_register_device(idev_touch);
-	if (err)
-		return dev_err_probe(dev, err, "Failed to register touch input device\n");
-
-	err = input_register_device(idev_headset);
-	if (err)
-		return dev_err_probe(dev, err, "Failed to register headset input device\n");
 
 	return 0;
 }
